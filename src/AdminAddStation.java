e496a74be8f8613f4479412e4cd3ce3719191cb0
munevveru <41445422+munevveru@users.noreply.github.com>
2018-07-22 18:58:21 +0300

    Update AdminAddStation.java

--- src/AdminAddStation.java
+++ src/AdminAddStation.java
@@ -1,4 +1,7 @@
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
@@ -14,6 +17,7 @@ public class AdminAddStation extends JPanel {
 	
 	private JButton saveStationButton;
 	private JButton editStationButton;
+	private JButton backHomeButton;
 	
 	private JTextField stationNoField;
 	private JTextField stationNameField;
@@ -24,10 +28,10 @@ public class AdminAddStation extends JPanel {
 	
 	private JPanel centerLabels;
 	private JPanel centerFields;
+	private JPanel total;
 	
 	public AdminAddStation()
 	{
-		setLayout(new GridLayout(3,1));
 		createComponents();
 	}
 	
@@ -38,7 +42,8 @@ public class AdminAddStation extends JPanel {
 		stationName = new JLabel("Station Name");
 		
 		saveStationButton = new JButton("Save");
-		editStationButton = new JButton("Edit Route");
+		editStationButton = new JButton("Edit Station");
+		backHomeButton = new JButton("Back to Home Page");
 		
 		stationNoField = new JTextField("Station No");
 		stationNameField = new JTextField("Station Name");
@@ -47,7 +52,8 @@ public class AdminAddStation extends JPanel {
 		center = new JPanel(new GridLayout(1,2));
 		centerLabels = new JPanel(new GridLayout(2,1));
 		centerFields = new JPanel(new GridLayout(2,1));
-		last = new JPanel(new GridLayout(2,1));
+		last = new JPanel(new GridLayout(3,1));
+		total = new JPanel(new GridLayout(3,1));
 		
 		first.add(addRoute);
 		centerLabels.add(stationNo);
@@ -61,12 +67,20 @@ public class AdminAddStation extends JPanel {
 		center.add(centerLabels);
 		center.add(centerFields);
 		
+		saveStationButton.addActionListener(new listener());
+		editStationButton.addActionListener(new listener());
+		backHomeButton.addActionListener(new listener());
+		
 		last.add(saveStationButton);
 		last.add(editStationButton);
+		last.add(backHomeButton);
 		
-		add(first);
-		add(center);
-		add(last);
+		total.add(first);
+		total.add(center);
+		total.add(last);
+	
+		setLayout(new BorderLayout());
+		add(total);
 		
 	}
 	
@@ -92,6 +106,47 @@ public class AdminAddStation extends JPanel {
 		public void focusLost(FocusEvent e) {
 			// TODO Auto-generated method stub
 			
+		}
+	}
+	
+	public String getStationName()
+	{
+		return stationNameField.getText();
+	}
+	
+	public int getStationNo()
+	{
+		return Integer.parseInt(stationNoField.getText());
+	}
+	public class listener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			if ( e.getSource() == saveStationButton ) {
+				Station newStation = new Station(getStationName(), getStationNo());	
+				viewer.stations.add(newStation);
+				viewer.stationNames.add(newStation.getStationName());
+				AdminAddStation newAdminAddStationPage = new AdminAddStation();
+			
+				removeAll();
+				add(newAdminAddStationPage);
+				revalidate();
+				repaint();
+			} 
+			
+			else if(e.getSource() == backHomeButton) {
+				AdminHome adminHomePage = new AdminHome();
+				
+				removeAll();
+				add(adminHomePage);
+				revalidate();
+				repaint();
+			}
+			
+		
+//			 if {
+//				//EDIT PANELI OLUŞTUR SONRA YAP 
+//			}
 		}
 	}
 }
